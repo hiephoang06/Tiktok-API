b3bb45fa45ec03c5b7a68e4d66ec3dabf0720e1c
HinPi <lmhiep.peel@gmail.com>
2022-10-30 12:42:42 +0700

    upload video

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
-import express from 'express'
-const app = express()
-const port = 3000
+import express from 'express';
+import { connect } from '../config/db/index.js';
+import { routes } from '../routes/index.js';
+import bodyParser from 'body-parser';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const app = express();
+const port = 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(path.dirname(__dirname), 'public')));
+
+connect();
+routes(app);
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
