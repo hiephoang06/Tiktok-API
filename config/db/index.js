a61e9f3e1976ed3eb449d2ef00bafc0c24c10a4f
HinPi <lmhiep.peel@gmail.com>
2023-01-02 16:59:15 +0700

    fix bug

--- config/db/index.js
+++ config/db/index.js
@@ -1,9 +1,9 @@
 import mongoose from 'mongoose';
 
-export const connect = async () => {
+export const connect = () => {
   try {
-    const url = 'mongodb+srv://kugon_2001:minhHiep1009@cluster0.ggrhtyq.mongodb.net/tiktok_dev';
-    await mongoose.connect(url);
+    const url = 'mongodb+srv://kugon_2001:minhHiep1009@cluster0.ggrhtyq.mongodb.net/tiktok_dev?retryWrites=true&w=majority';
+    mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
     console.log('connect success');
   } catch (e) {
     console.log('connect failed', e);
